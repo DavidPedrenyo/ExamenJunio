1d86b20d16b46698f57d0d68c7ff862f459f8b7d
david <david.pedreno93@gmail.com>
2023-05-17 11:02:27 +0200

    Añadido JavaDoc constructor clase Animal

--- src/granja/Main.java
+++ src/granja/Main.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package granja;
 
 import animales.Animal;
@@ -12,6 +8,11 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
+
+/**
+ * 
+ * @author david
+ */
 public class Main {
 
     public static void main(String[] args) {
